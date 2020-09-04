ab1f117ed3101320e136beacb5e3e47abea2ffeb
hanspeter schlapbach <hpsof@users.noreply.github.com>
2020-09-04 17:45:24 +0200

    auto-build switchplus eingerichtet

--- hugo.cmd
+++ hugo.cmd
@@ -1,2 +1,5 @@
 
+rem --baseURL=http://dev.kijumed.ch/             fuer deploy auf switchplus 
+rem --baseURL=http://hpsof.sofim.ch/lernen_hugo/ fuer deploy auf github
+rem --baseURL=localhost:1313                     fuer local hpsof
 ..\hugo_extended\hugo.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
