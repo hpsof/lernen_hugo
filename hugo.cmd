ecc4499fee1162ab8a5316e9b629cc92c437b595
hanspeter schlapbach <hpsof@users.noreply.github.com>
2020-09-08 21:13:04 +0200

    commit nach build switch

--- hugo.cmd
+++ hugo.cmd
@@ -1,5 +1,5 @@
 
 rem --baseURL=http://dev.kijumed.ch/             fuer deploy auf switchplus 
 rem --baseURL=http://hpsof.sofim.ch/lernen_hugo/ fuer deploy auf github
-rem --baseURL=localhost:1313                     fuer local hpsof
+rem --baseURL=localhost:1313                     fuer local hpsof via hugo.cmd server
 ..\hugo_extended\hugo.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
